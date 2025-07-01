347f4c26d047d1def7ea130314f85678e6bdb72a
AkashCreates <akashmaityofficial@gmail.com>
2025-07-01 13:49:48 +0530

    complete

--- js/script.js
+++ js/script.js
@@ -7,9 +7,14 @@ const weatherIcon = document.querySelector(".weather-icon")
 
 async function checkWeather(city){
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
-    var data = await response.json();
 
-    console.log(data);
+    if(response.status == 404){
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+    }else{
+         var data = await response.json();
+
+     
 
     document.querySelector(".city").innerHTML = data.name;
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
@@ -36,11 +41,20 @@ async function checkWeather(city){
     }
 
     document.querySelector(".weather").style.display = "block";
+     document.querySelector(".error").style.display = "none";
+    }
+
+   
 
 }
 cityBtn.addEventListener("click" , () => {
     checkWeather(cityName.value);
 })
+cityName.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+        checkWeather(cityName.value);
+    }
+});
 
 
 
